bc3c321b15b95c8b632c213e8f4534d04c6f27f3
TH1922 <ginvolka1996@gmail.com>
2023-09-14 21:34:50 +0700

    bt02

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./App.css";
+import  Header  from './components/Header';
+import Content from './components/Content';
+import Aside from './components/Aside';
+import Box from './components/Box';
+import Footer from './components/Footer'; 
+
+const App = () => {
+  return (
+    <div className="head">
+      <Header />
+      <div className='body'>
+        <div className="body-left">
+          <Content />
+        </div>
+        <div className="body-right">
+          <Aside />
+        </div>
+      </div>
+      <div className='boxx'>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
